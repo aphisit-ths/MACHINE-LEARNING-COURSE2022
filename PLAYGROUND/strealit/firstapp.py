24cd4665485fb0ea40800f6e93d07af0c616d71e
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-03-15 01:34:32 +0700

    strea

---
+++ PLAYGROUND/strealit/firstapp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+
+
+
+
+st.title('HEY')
